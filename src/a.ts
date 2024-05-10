b1bd38bb21ede2a5a72aafd6a3a09116733acd95
Manavatelemensis <168632844+Manavatelemensis@users.noreply.github.com>
2024-05-10 07:11:34 +0000

    Updated

---
+++ src/a.ts
@@ -0,0 +1,55 @@
+// let x:number
+// x=1010
+// console.log(x)
+
+// //Types Of Typescript
+// // 1.number
+// // 2.string
+// // 3.boolean
+// // 4.null
+// // 5.undefined
+
+// //function to greet user
+
+// function greet(name:string){
+//     console.log("Hello " +name)
+// }
+
+// greet("Manav")
+
+// //Type Interface
+
+
+// function sum(num:number,num2:number):number{
+//     return num+num2;
+// }
+// const value=sum(5,10)
+// console.log(value)
+
+// function voting(a:number):boolean{
+//     if(a>18){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+
+// const votingAge=voting(18)
+// // console.log(votingAge)
+
+
+
+// function first():void{
+//     console.log("Hiii")
+// }
+
+// function second(first:()=>void){
+//     setTimeout(first, 1000);
+// }
+
+// second(first)
+
+const
+
+function isLegal(user:num)
+
